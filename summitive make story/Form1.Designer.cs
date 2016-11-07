f89629094d6982de836bcd3819cf49fd74cc2d37
McDonsFries <mikaracz083@ed.amdsb.ca>
2016-11-07 13:35:40 -0500

    Add project files.

---
+++ summitive make story/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace summitive_make_story
+{
+    partial class Story
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.firstLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.thirdLabel = new System.Windows.Forms.Label();
+            this.mLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.spaceLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(227, 9);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(459, 389);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // firstLabel
+            // 
+            this.firstLabel.AutoSize = true;
+            this.firstLabel.BackColor = System.Drawing.Color.Transparent;
+            this.firstLabel.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstLabel.Location = new System.Drawing.Point(12, 36);
+            this.firstLabel.Name = "firstLabel";
+            this.firstLabel.Size = new System.Drawing.Size(70, 16);
+            this.firstLabel.TabIndex = 4;
+            this.firstLabel.Text = "Press \'M\'";
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.AutoSize = true;
+            this.secondLabel.BackColor = System.Drawing.Color.Transparent;
+            this.secondLabel.Font = new System.Drawing.Font("Trajan Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.secondLabel.Location = new System.Drawing.Point(12, 151);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(65, 16);
+            this.secondLabel.TabIndex = 5;
+            this.secondLabel.Text = "Press \'B\'";
+            // 
+            // thirdLabel
+            // 
+            this.thirdLabel.AutoSize = true;
+            this.thirdLabel.BackColor = System.Drawing.Color.Transparent;
+            this.thirdLabel.Font = new System.Drawing.Font("Trajan Pro", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.thirdLabel.Location = new System.Drawing.Point(12, 273);
+            this.thirdLabel.Name = "thirdLabel";
+            this.thirdLabel.Size = new System.Drawing.Size(144, 16);
+            this.thirdLabel.TabIndex = 6;
+            this.thirdLabel.Text = "Press the spacebar";
+            // 
+            // mLabel
+            // 
+            this.mLabel.AutoSize = true;
+            this.mLabel.Location = new System.Drawing.Point(18, 73);
+            this.mLabel.Name = "mLabel";
+            this.mLabel.Size = new System.Drawing.Size(0, 13);
+            this.mLabel.TabIndex = 7;
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(19, 189);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(0, 13);
+            this.bLabel.TabIndex = 8;
+            // 
+            // spaceLabel
+            // 
+            this.spaceLabel.AutoSize = true;
+            this.spaceLabel.Location = new System.Drawing.Point(21, 314);
+            this.spaceLabel.Name = "spaceLabel";
+            this.spaceLabel.Size = new System.Drawing.Size(0, 13);
+            this.spaceLabel.TabIndex = 9;
+            // 
+            // Story
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::summitive_make_story.Properties.Resources.sidewalk;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(697, 407);
+            this.Controls.Add(this.spaceLabel);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.mLabel);
+            this.Controls.Add(this.thirdLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.firstLabel);
+            this.Controls.Add(this.outputLabel);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "Story";
+            this.Text = "Summitive Story";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label firstLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label thirdLabel;
+        private System.Windows.Forms.Label mLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.Label spaceLabel;
+    }
+}
+
---
+++ summitive make story/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace summitive_make_story
+{
+    public partial class Story : Form
+    {
+        int scene = 0;  // tracks what part of the game the user is at
+
+        public Story()
+        {
+            InitializeComponent();
+            //display initial message and options
+            outputLabel.Text = "Its the end of the day and you start to walk home from school. Do you walk home with your friends, your crush or alone?";
+            mLabel.Text = "Alone";
+            bLabel.Text = "Friends";
+            spaceLabel.Text = "Crush";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            /// First decision
+            if (e.KeyCode == Keys.M)      
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+            }
+            else if (e.KeyCode == Keys.B)  
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 1)
+                {
+                    scene = 5;
+                }
+
+            }
+            else if (e.KeyCode == Keys.Space) 
+            {
+                if (scene == 0)
+                {
+                    scene = 3;
+                }
+                else if (scene == 1)
+                {
+                    scene = 5;
+                }
+
+            }
+
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:  //start scene
+                    outputLabel.Text = "Do you walk alone, with your friends or your crush?";
+                    mLabel.Text = "Alone";
+                    bLabel.Text = "With your crush";
+                    spaceLabel.Text = "With your friends";
+                    break;
+                case 1:
+                    outputLabel.Text = "You walk home by yourself sad and then find a cat  on your way to the ice cream store to comfort your feelings. Do you keep the cat or let it be?";
+                    mLabel.Text = "Keep it";
+                    bLabel.Text = "Leave it be";
+                    spaceLabel.Text = "";
+                    break;
+                case 2:
+                    outputLabel.Text = "Your crush asks if you want to hang out at their place.  Do you go or not?";
+                    mLabel.Text = "Yes";
+                    bLabel.Text = "No";
+                    spaceLabel.Text = "";
+                    break;
+                case 3:
+                    outputLabel.Text = "Your friends invite you to tims with them. Do you go or not?";
+                    mLabel.Text = "Yes";
+                    bLabel.Text = "No";
+                    spaceLabel.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = "They want to know if you want to watch Netflix or just hang out. Do you choose Netflix or hangout?";
+                    mLabel.Text = "Netflix";
+                    bLabel.Text = "Hangout";
+                    spaceLabel.Text = "";
+                    break;
+                case 5:
+                    outputLabel.Text = "Your one friend pulls out a cigarette before entering the tims and asks you if you want one. Do you take a cigarette?";
+                    mLabel.Text = "Yes";
+                    bLabel.Text = "No";
+                    spaceLabel.Text = "";
+                    break;
+                case 6:
+                    outputLabel.Text = "At the pet store you get your new pet a name tag and a collar. You see a missing pet sign with your new friend on it. Do you return it or run?";
+                    mLabel.Text = "Return it like a good person";
+                    bLabel.Text = "RUN FORREST RUN";
+                    spaceLabel.Text = "";
+                    break;
+                case 7:
+                    outputLabel.Text = "They ask what you want to watch. What do you say?";
+                    mLabel.Text = "Supernatural";
+                    bLabel.Text = "The Vampire Diaries";
+                    spaceLabel.Text = "The Saw movies";
+                    break;
+                case 8:
+                    outputLabel.Text = "What do you buy?";
+                    mLabel.Text = "Doughnut";
+                    bLabel.Text = "Timbits";
+                    spaceLabel.Text = "Ice Capp";
+                    break;
+                case 9:
+                    outputLabel.Text = "You get scared and they put their arm around you. What do you do?";
+                    mLabel.Text = "Run";
+                    bLabel.Text = "Let them";
+                    spaceLabel.Text = "";
+                    break;
+
+            }
+        }
+    }
+}
