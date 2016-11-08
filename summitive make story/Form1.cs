dcfa80614260d42f90b796889f8b689284e6cf7b
McDonsFries <mikaracz083@ed.amdsb.ca>
2016-11-08 13:32:29 -0500

    ugh

--- summitive make story/Form1.cs
+++ summitive make story/Form1.cs
@@ -13,6 +13,8 @@ namespace summitive_make_story
     public partial class Story : Form
     {
         int scene = 0;  // tracks what part of the game the user is at
+        Random randgen = new Random();
+        int rand = 0;          
 
         public Story()
         {
@@ -43,96 +45,177 @@ namespace summitive_make_story
                 {
                     scene = 6;
                 }
+                else if (scene == 3)
+                {
+                    scene = 13;
+                }
+                else if (scene == 13)
+                {
+                    rand = randgen.Next(1,4);
+                    scene = 15;
+
+                }
+                else if (scene == 13)
+                {
+                    scene = 0;
+                }
+                else if (scene == 2)
+                {
+                    scene = 8;
+                }
+                else if (scene == 6 )
+                {
+                    scene = 0 ;
+                }
+                else if (scene == 8)
+                {
+                    scene = 9;
+                }
             }
             else if (e.KeyCode == Keys.B)  
             {
                 if (scene == 0)
                 {
-                    scene = 2;
-                }
-                else if (scene == 1)
-                {
-                    scene = 5;
-                }
-
-            }
-            else if (e.KeyCode == Keys.Space) 
-            {
-                if (scene == 0)
-                {
                     scene = 3;
                 }
+                else if (scene == 3)
+                {
+                    scene = 1;
+                }
                 else if (scene == 1)
                 {
                     scene = 5;
                 }
+                else if (scene == 5)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    scene = 1;
+                }
+                else if (scene == 4)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 0;
+                }
+                else if (scene == 8)
+                {
+                    scene = 11;
+                }
+                else if (scene == 11)
+                {
+                    scene = 12;
+                }
+                else if (scene == 12)
+                {
+                    scene = 0;
+                }
 
-            }
 
-
-            /// Display text and game options to screen based on the current scene
-            switch (scene)
+                else if (e.KeyCode == Keys.Space) 
             {
-                case 0:  //start scene
-                    outputLabel.Text = "Do you walk alone, with your friends or your crush?";
-                    mLabel.Text = "Alone";
-                    bLabel.Text = "With your crush";
-                    spaceLabel.Text = "With your friends";
-                    break;
-                case 1:
-                    outputLabel.Text = "You walk home by yourself sad and then find a cat  on your way to the ice cream store to comfort your feelings. Do you keep the cat or let it be?";
-                    mLabel.Text = "Keep it";
-                    bLabel.Text = "Leave it be";
-                    spaceLabel.Text = "";
-                    break;
-                case 2:
-                    outputLabel.Text = "Your crush asks if you want to hang out at their place.  Do you go or not?";
-                    mLabel.Text = "Yes";
-                    bLabel.Text = "No";
-                    spaceLabel.Text = "";
-                    break;
-                case 3:
-                    outputLabel.Text = "Your friends invite you to tims with them. Do you go or not?";
-                    mLabel.Text = "Yes";
-                    bLabel.Text = "No";
-                    spaceLabel.Text = "";
-                    break;
-                case 4:
-                    outputLabel.Text = "They want to know if you want to watch Netflix or just hang out. Do you choose Netflix or hangout?";
-                    mLabel.Text = "Netflix";
-                    bLabel.Text = "Hangout";
-                    spaceLabel.Text = "";
-                    break;
-                case 5:
-                    outputLabel.Text = "Your one friend pulls out a cigarette before entering the tims and asks you if you want one. Do you take a cigarette?";
-                    mLabel.Text = "Yes";
-                    bLabel.Text = "No";
-                    spaceLabel.Text = "";
-                    break;
-                case 6:
-                    outputLabel.Text = "At the pet store you get your new pet a name tag and a collar. You see a missing pet sign with your new friend on it. Do you return it or run?";
-                    mLabel.Text = "Return it like a good person";
-                    bLabel.Text = "RUN FORREST RUN";
-                    spaceLabel.Text = "";
-                    break;
-                case 7:
-                    outputLabel.Text = "They ask what you want to watch. What do you say?";
-                    mLabel.Text = "Supernatural";
-                    bLabel.Text = "The Vampire Diaries";
-                    spaceLabel.Text = "The Saw movies";
-                    break;
-                case 8:
-                    outputLabel.Text = "What do you buy?";
-                    mLabel.Text = "Doughnut";
-                    bLabel.Text = "Timbits";
-                    spaceLabel.Text = "Ice Capp";
-                    break;
-                case 9:
-                    outputLabel.Text = "You get scared and they put their arm around you. What do you do?";
-                    mLabel.Text = "Run";
-                    bLabel.Text = "Let them";
-                    spaceLabel.Text = "";
-                    break;
+               if (scene == 0)
+               {
+                   scene = 2;
+               }
+
+               else if (scene == 9)
+               {
+                   scene = 10;
+               }
+                    else if (scene == 10)
+                    {
+                        scene = 11;
+                    }
+
+                }
+
+
+                /// Display text and game options to screen based on the current scene
+                switch (scene)
+                {
+                    case 0:  //start scene
+                        outputLabel.Text = "Do you walk alone, with your friends or your crush?";
+                        mLabel.Text = "Alone";
+                        bLabel.Text = "With your crush";
+                        spaceLabel.Text = "With your friends";
+                        break;
+                    case 1:
+                        outputLabel.Text = "You walk home by yourself sad and then find a cat  on your way to the ice cream store to comfort your feelings. Do you keep the cat or let it be?";
+                        mLabel.Text = "Keep it";
+                        bLabel.Text = "Leave it be";
+                        spaceLabel.Text = "";
+                        break;
+                    case 2:
+                        outputLabel.Text = "Your crush asks if you want to hang out at their place.  Do you go or not?";
+                        mLabel.Text = "Yes";
+                        bLabel.Text = "No";
+                        spaceLabel.Text = "";
+                        break;
+                    case 3:
+                        outputLabel.Text = "Your friends invite you to tims with them. Do you go or not?";
+                        mLabel.Text = "Yes";
+                        bLabel.Text = "No";
+                        spaceLabel.Text = "";
+                        break;
+                    case 4:
+                        outputLabel.Text = "They want to know if you want to watch Netflix or just hang out. Do you choose Netflix or hangout?";
+                        mLabel.Text = "Netflix";
+                        bLabel.Text = "Hangout";
+                        spaceLabel.Text = "";
+                        break;
+                    case 5:
+                        outputLabel.Text = "Your one friend pulls out a cigarette before entering the tims and asks you if you want one. Do you take a cigarette?";
+                        mLabel.Text = "Yes";
+                        bLabel.Text = "No";
+                        spaceLabel.Text = "";
+                        break;
+                    case 6:
+                        outputLabel.Text = "At the pet store you get your new pet a name tag and a collar. You see a missing pet sign with your new friend on it. Do you return it or run?";
+                        mLabel.Text = "Return it like a good person";
+                        bLabel.Text = "RUN FORREST RUN";
+                        spaceLabel.Text = "";
+                        break;
+                    case 7:
+                        outputLabel.Text = "They ask what you want to watch. What do you say?";
+                        mLabel.Text = "Supernatural";
+                        bLabel.Text = "The Vampire Diaries";
+                        spaceLabel.Text = "The Saw movies";
+                        break;
+                    case 8:
+                        outputLabel.Text = "What do you buy?";
+                        mLabel.Text = "Doughnut";
+                        bLabel.Text = "Timbits";
+                        spaceLabel.Text = "Ice Capp";
+                        break;
+                    case 9:
+                        outputLabel.Text = "You get scared and they put their arm around you. What do you do?";
+                        mLabel.Text = "Run";
+                        bLabel.Text = "Let them";
+                        spaceLabel.Text = "";
+                        break;
+                    case 15:
+                        if (rand == 1)
+                        {
+                            outputLabel.Text = "timbit";
+                        }
+                        else if (rand == 2)
+                        {
+                            outputLabel.Text = "donut";
+                        }
+                        else
+                        {
+                            outputLabel.Text = "coffee";
+                        }
+                        break;
+                        mLabel.Text = "Yes";
+                        bLabel.Text = "No";
+                        spaceLabel.Text = "";
+                }
 
             }
         }
